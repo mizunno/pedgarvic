a6349c1268d10337c817fa0e7eef893c499641e7
Pedro García Victoria <mizunno@Pedros-MacBook-Pro.local>
2024-03-11 12:34:36 +0100

    Change newton picture

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 USER=mizunno
 HOST=pedgarvic.me
 DIR=/var/www/pedgarvic.me/html/
-SSH_KEY_PATH=/home/mizunno/.ssh/pedgarvic
+SSH_KEY_PATH=~/.ssh/pedgarvic
 
 hugo
 rsync -avz --delete public/ -e "ssh -i ${SSH_KEY_PATH}" ${USER}@${HOST}:${DIR}
